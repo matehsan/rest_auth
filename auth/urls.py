ed3c18d92b6701b4fdbbb8d5dbf75a34dc89c979
ehsangun <matari.ehsan@yahoo.com>
2020-09-16 10:16:30 +0430

    4 endpoint added

--- auth/urls.py
+++ auth/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+# from rest_framework.routers import DefaultRouter
 from . import views
 
-router = DefaultRouter()
-router.register('profile', views.UserProfileViewSet)
+# router = DefaultRouter()
+# router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('login/', views.UserLoginApiView.as_view()),
-    path('', include(router.urls))
+    path('user/', views.UserProfile.as_view()),
+    path('create/', views.CreateUser.as_view()),
+    path('user/password/', views.PasswordUpdate.as_view()),
+
+    # path('', include(router.urls))
 ]

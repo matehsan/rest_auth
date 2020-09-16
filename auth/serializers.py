1b27bcc61803d101d9f06b20ef8a42ec0f9e54d3
ehsangun <matari.ehsan@yahoo.com>
2020-09-16 10:19:03 +0430

    add password change, remove unnecessary functions

--- auth/serializers.py
+++ auth/serializers.py
@@ -7,32 +7,26 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     """Serializes a user profile object"""
 
-    def create(self, validated_data):
-        """Create and return a new user"""
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-
-        return user
+    # def create(self, validated_data):
+    #     """Create and return a new user"""
+    #     user = User.objects.create_user(
+    #         username=validated_data['username'],
+    #         email=validated_data['email'],
+    #         password=validated_data['password']
+    #     )
+    #
+    #     return user
 
     # def update(self, instance, validated_data):
-    #     user = User.objects.filter(username=instance)
-    #     print(user)
     #
+    #     if 'username' in validated_data:
+    #         instance.username = validated_data['username']
+    #     if 'email' in validated_data:
+    #         instance.email = validated_data['email']
+    #     if 'password' in validated_data:
+    #         instance.password = make_password(validated_data['password'])
+    #     instance.save()
     #     return instance
-
-    def update(self, instance, validated_data):
-
-        if 'username' in validated_data:
-            instance.username = validated_data['username']
-        if 'email' in validated_data:
-            instance.email = validated_data['email']
-        if 'password' in validated_data:
-            instance.password = make_password(validated_data['password'])
-        instance.save()
-        return instance
 
     class Meta:
         model = User
@@ -43,3 +37,12 @@ class UserSerializer(serializers.ModelSerializer):
                 'style': {'input_type': 'password'}
             }
         }
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    model = User
+
+    old_password = serializers.CharField(required=True, write_only=True,
+                                         style={'input_type': 'password', 'placeholder': 'Old Password'})
+    new_password = serializers.CharField(required=True, write_only=True,
+                                         style={'input_type': 'password', 'placeholder': 'New Password'})

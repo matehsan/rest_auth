ec8de921a32dcda55bc6e3fad2c111cd36fff1ca
ehsangun <matari.ehsan@yahoo.com>
2020-09-22 16:07:44 +0330

    tests

--- auth/tests/test_user.py
+++ auth/tests/test_user.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 from rest_framework.test import APIClient, APIRequestFactory
 from rest_framework.test import force_authenticate
@@ -16,8 +17,14 @@ class UserTests(TestCase):
     """user test"""
 
     def setUp(self):
-        # TODO create user
         self.client = APIClient()
+
+        self.user_data = {
+            "username": 'user',
+            "email": 'user@user.com',
+            "password": 'user123456'
+        }
+        create_user(**self.user_data)
 
     def test_create_user(self):
         """create user tests"""
@@ -34,49 +41,37 @@ class UserTests(TestCase):
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data['data'])
 
-    def test_user_exists(self):
-        payload = {
-            "username": 'test',
-            "email": 'test@test.com',
-            "password": '123456'
-        }
-
-        create_user(**payload)
-
-        res = self.client.post('/api/create/', payload)
+    def test_create_user_exists(self):
+        res = self.client.post('/api/create/', self.user_data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_token_login_ok(self):
         """token auth test"""
         payload = {
-            "username": 'test',
-            "password": '123456'
+            "username": self.user_data['username'],
+            "password": self.user_data['password']
         }
-        create_user(**payload)
+        user = get_user_model().objects.get(username=self.user_data['username'])
         res = self.client.post('/api/login/', payload)
 
         self.assertIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        # TODO token dorost bashe
+        # check token
+        self.assertEqual(res.data['token'], Token.objects.get(user=user).key)
 
     def test_token_login_not_ok(self):
-        payload = {
-            "username": 'test',
-            "password": '123456'
-        }
-        create_user(**payload)
-        wrong_password_payload = {
-            "username": 'test',
+        wrong_user_payload = {
+            "username": 'user',
             "password": 'wrong'
         }
-        res = self.client.post('/api/login/', wrong_password_payload)
+        res = self.client.post('/api/login/', wrong_user_payload)
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_token_login_no_user(self):
         payload = {
-            "username": 'test',
+            "username": 'nouser',
             "password": '123456'
         }
         res = self.client.post('/api/login/', payload)
@@ -84,34 +79,26 @@ class UserTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_get_user_info(self):
-        payload = {
-            "username": 'test',
-            "email": 'test@test.com',
-            "password": '123456'
-        }
-        create_user(**payload)
-        user = get_user_model().objects.get(username=payload['username'])
+        user = get_user_model().objects.get(username=self.user_data['username'])
 
         user_info_request_factory = APIRequestFactory()
         user_profile_view = UserProfile.as_view()
 
         request = user_info_request_factory.get('/api/user/')
-        # print(request.user)
+
         force_authenticate(request, user=user)
-        # print(request)
+
         res = user_profile_view(request)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.data[0]['username'], payload['username'])
+        self.assertEqual(res.data[0]['username'], self.user_data['username'])
         # TODO hameye halat
 
-    def test_token_base_user_info(self):
+    def test_get_user_info_token_base(self):
         payload = {
-            "username": 'test',
-            "email": 'test@test.com',
-            "password": '123456'
+            "username": self.user_data['username'],
+            "password": self.user_data['password']
         }
-        create_user(**payload)
 
         res = self.client.post('/api/login/', payload)
         token = res.data['token']
@@ -122,13 +109,15 @@ class UserTests(TestCase):
         self.assertEqual(res2.status_code, status.HTTP_200_OK)
         self.assertEqual(res2.data[0]['username'], payload['username'])
 
+    def test_get_user_info_unauthorized(self):
+        res = self.client.get('/api/user/')
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
     def test_change_password(self):
         payload = {
-            "username": 'test',
-            "email": 'test@test.com',
-            "password": '123456'
+            "username": self.user_data['username'],
+            "password": self.user_data['password']
         }
-        create_user(**payload)
 
         res = self.client.post('/api/login/', payload)
         token = res.data['token']
@@ -136,10 +125,34 @@ class UserTests(TestCase):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
 
         passwords = {
-            'old_password': '123456',
+            'old_password': self.user_data['password'],
             'new_password': 'salam123'
         }
 
         res2 = self.client.put('/api/user/password/', passwords)
 
         self.assertEqual(res2.status_code, status.HTTP_200_OK)
+
+    def test_change_password_wrong_password(self):
+        payload = {
+            "username": self.user_data['username'],
+            "password": self.user_data['password']
+        }
+
+        res = self.client.post('/api/login/', payload)
+        token = res.data['token']
+
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+
+        passwords = {
+            'old_password': 'wrong_password',
+            'new_password': 'no_matter'
+        }
+
+        res2 = self.client.put('/api/user/password/', passwords)
+
+        self.assertEqual(res2.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_change_password_unauthorized(self):
+        res = self.client.put('/api/user/password/')
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)

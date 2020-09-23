844a7d8c6efe45563f69fbb4f63d9dcef60c5ea3
ehsangun <matari.ehsan@yahoo.com>
2020-09-23 14:19:28 +0330

    initial commit

---
+++ exchange/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+# router.register('profile', views.UserProfileViewSet)
+
+urlpatterns = [
+    # path('', include(router.urls))
+]

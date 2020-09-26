9eef79e1af1ae2e1015a5cf87a0cae82154bc19a
ehsangun <matari.ehsan@yahoo.com>
2020-09-26 17:44:54 +0330

    analysts endpoint added

--- rest_auth/urls.py
+++ rest_auth/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('auth.urls'))
+    path('api/', include('auth.urls')),
+    path('analysts', include('analyzer.urls'))
 
 ]
